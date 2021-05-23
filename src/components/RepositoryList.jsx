585bc48e7193adb1854a9afb74dc82f72e863701
ottonsam <ottonsam@gmail.com>
2021-05-23 18:48:38 -0300

    Create component RepositoryList and import in App

---
+++ src/components/RepositoryList.jsx
@@ -0,0 +1,16 @@
+export function RepositoryList() {
+  return (
+    <section className="repository-list">
+      <h1>Lista de repositórios</h1>
+
+      <ul>
+        <li>
+          <strong>Unform</strong>
+          <p>Forms in reactJS</p>
+
+          <a href="#">Acessar repositório</a>
+        </li>
+      </ul>
+    </section>
+  )
+}
