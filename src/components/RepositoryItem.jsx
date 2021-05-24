de99a921d56f4196ca9c6220b6edcf8cc13a4b1a
ottonsam <ottonsam@gmail.com>
2021-05-23 21:11:09 -0300

    Set default mensage

--- src/components/RepositoryItem.jsx
+++ src/components/RepositoryItem.jsx
@@ -3,7 +3,7 @@ export function RepositoryItem(props) {
 
   return (
     <li>
-      <strong>{ props.repository || 'Unform' }</strong>
+      <strong>{ props.repository ?? 'Default' }</strong>
       <p>Forms in reactJS</p>
 
       <a href="#">Acessar repositório</a>
